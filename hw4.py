c3de335b7a4b119b9806fc9a29506c27e791fc15
kevinterwilliger <kevint3@illinois.edu>
2018-03-07 21:36:10 -0600

    first commit; probably all wrong

--- hw4.py
+++ hw4.py
@@ -4,27 +4,36 @@ Homework 4 - Linked Lists
 To understand recursion, you must first understand recursion.
 """
 
+
 class Node(object):
     """
     Class for linked list node.
     """
+
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
+
 
 def list_length(head):
     """
     Given a linked list, returns the length ofthe linked list.
     Suppose the existing linked list looks like this: 3 -> 5 -> 9 -> 6
     Then the length is 4.
-    
+
     Input:
         head - the head Node of a linked list
-    
+
     Returns:
         (int) The length of the linked list
     """
-    pass
+    i = 0
+    while(True):
+        if (head.next_node != None):
+            head = head.next_node
+            i += 1
+        else:
+            return i
 
 
 def add_head(head, data):
@@ -45,7 +54,11 @@ def add_head(head, data):
     Returns:
         (Node) the head of the new linked list that includes 'data'
     """
-    pass
+    data.next_node = head
+    node = head.next_node
+    for i in range(list_length):
+        node = node.next_node
+    return data
 
 
 def add_tail(head, data):
@@ -66,37 +79,62 @@ def add_tail(head, data):
     Returns:
         (Node) the head of the new linked list that includes 'data'
     """
-    pass
+    node = head.next_node
+    while(True):
+        if (node.next_node != None):
+            node = node.next_node
+        else:
+            break
+    node.next_node = data
+    return data
 
 
 def remove_position(head, position):
     """
-    Given the head of a linked list, removes the element of the 
+    Given the head of a linked list, removes the element of the
     specified position. If the position is greater than the length
     of the linked list, do not remove anything.
 
     Input:
         head - the head of the linked list.
         position - the position at which to remove an element.
-    
+
     Output:
         the head of the linked list.
     """
-    pass
+    if (position > 1):
+        node = head.next_node
+        for i in range(position):
+            node = node.next_node
+        return node
+    elif (position == 1):
+        return head.next_node
+    else:
+        return head
 
 
 def find_cycle(head):
     """
     Given the head of a linked list, determines whether or not there
     is a cycle in the linked list.
-    
+
     Ex. suppose we have a linked list 3 -> 4 -> 7, and the last node points
     back to the node labeled 4. Then this linked list has a cycle.
-    
+
     Input:
         head - the head of the linked list.
-    
+
     Output:
         (bool) whether or not there is a cycle in the linked list.
     """
-    pass
+    one = head
+    two = head
+    while (two != None)
+        one = one.next_node
+        if (two.next_node != None):
+            two = two.next_node.next_node
+        else:
+            return False
+        if one is two:
+            return True
+    return False
